379fdccc8e813125308b3e1ccc1f2874ca5292ab
15915940450 <15915940450@139.com>
2017-05-11 18:57:22 +0800

    m1396ck

---
+++ stop_smoking/js/m1396ck.js
@@ -0,0 +1,39 @@
+function ajaxGET(url,callback){
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function(){
+    if(xmlhttp.readyState===4 && xmlhttp.status===200){
+      callback(xmlhttp.responseText);
+    }
+  };
+  xmlhttp.open('GET',url,true);
+  xmlhttp.send();
+}
+
+var eleTmp=document.getElementById('tmp');
+var eleTbody=document.querySelector('#tmp table tbody');
+ajaxGET('http://m.1396ck.com/stat/5?client_lang=zh-tw&year=2017',function(data){
+  if(data){
+    var objData=JSON.parse(data);
+    var arrData=objData.items;
+    var arrTr=[];
+    var arrTd=[];
+    var arrWeek=['日','一','二','三','四','五','六'];
+    for(var i=0;i<arrData.length;i++){
+      var numY=Number(arrData[i].period.substring(0,4));
+      var numM=Number(arrData[i].period.substring(5,7))-1;
+      var numD=Number(arrData[i].period.substring(8,10));
+      var datePeriod=new Date(numY,numM,numD);  //var birthday = new Date(1995, 11, 17);
+      // alert(datePeriod);
+      arrTd[0]='<td>星期'+arrWeek[datePeriod.getDay()]+'</td>';
+      arrTd[1]='<td>'+arrData[i].period+'</td>';
+      arrTd[2]='<td>'+arrData[i].numbers.slice(-2)+'</td>';
+      arrTd[3]='<td>'+arrData[i].sx.slice(-1)+'</td>';
+      arrTd[4]='<td>'+arrData[i].wx.slice(-1)+'</td>';
+      var strTr='<tr>'+arrTd.join('')+'</tr>';
+      arrTr.push(strTr);
+    }
+    eleTbody.innerHTML=arrTr.join('');
+  }else{
+    eleTmp.innerHTML="無數據";
+  }
+});
